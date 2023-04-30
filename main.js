a3c43d46248ae85cada0f49a05e9fe2afdc4739e
DazzleFS <132039631+DazzleFS@users.noreply.github.com>
2023-04-30 16:14:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function fun()
+{
+    console.log("PROCCESSING");
+    window.location = "anotherpage.html";
+}
+
+function fun2()
+{
+    console.log("PROCCESSING");
+    window.location = "index.html";
+}
